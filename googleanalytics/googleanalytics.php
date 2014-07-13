753d1d1685b294c40905e8e2d9f87eae000ee8d8
dan-magebin <dan@magebin.com>
2014-07-14 11:32:06 +1200

    Update googleanalytics.php

--- googleanalytics/googleanalytics.php
+++ googleanalytics/googleanalytics.php
@@ -36,8 +36,8 @@ if (!defined('_PS_VERSION_'))
 	 {
 		 $this->name = 'googleanalytics';
 		 $this->tab = 'analytics_stats';
-		 $this->version = '1.0';
-		 $this->author = 'Dan Chen(dan@magebin.com)';
+		 $this->version = '0.9(beta)';
+		 $this->author = 'Dan(dan@magebin.com)';
 		 $this->need_instance = 1;
 		 $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
 		 $this->bootstrap = true;
@@ -45,7 +45,7 @@ if (!defined('_PS_VERSION_'))
 		 parent::__construct();
 
 		 $this->displayName = $this->l('Google Analytics');
-		 $this->description = $this->l('This is the GoogleAnalytics extension for Prestashop');
+		 $this->description = $this->l('This is the GoogleAnalytics extension for Prestashop, using enhanced e-commerce Google Analytics API');
 
 		 $this->confirmUninstall = $this->l('Are you sure you want to uninstall GoogleAnalytics?');
 
@@ -69,8 +69,7 @@ if (!defined('_PS_VERSION_'))
 			 !$this->registerHook('home') ||
 			 !$this->registerHook('productfooter') ||
 			 !$this->registerHook('shoppingCart') ||
-			 !$this->registerHook('top') ||
-			 !Configuration::updateValue('MYMODULE_NAME', 'my friend')
+			 !$this->registerHook('top') 
 		 )
 			 return false;
 
@@ -106,34 +105,9 @@ if (!defined('_PS_VERSION_'))
 		 return true;
 	 }
 
-	 /**
-	  * back office module configuration page content
-	  */
-	 public function getContent()
-	 {
-		$output = '';
-    if (Tools::isSubmit('submit'.$this->name))
-		{
-			$my_module_name = strval(Tools::getValue('MYMODULE_NAME'));
-			 if (!$my_module_name  || empty($my_module_name) || !Validate::isGenericName($my_module_name))
-				 $output .= $this->displayError( $this->l('Invalid Configuration value') );
-			 else
-			 {
-				 Configuration::updateValue('MYMODULE_NAME', $my_module_name);
-				 $output .= $this->displayConfirmation($this->l('Settings updated'));
-			 }
-		}
-    return $output.$this->displayForm();
-	 }
+	
 
-	 /**
-	  * hook page header to add CSS and JS files
-	  */
-	 public function hookDisplayHeader()
-	 {
-
-	 }
-
+	
 	 /**
 	  * back office return configuration form
 	  * @return mixed
@@ -143,26 +117,7 @@ if (!defined('_PS_VERSION_'))
 		 // Get default Language
 		 $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
 
-		 // Init Fields form array
-		 $fields_form[0]['form'] = array(
-			 'legend' => array(
-				 'title' => $this->l('Settings'),
-			 ),
-			 'input' => array(
-				 array(
-					 'type' => 'text',
-					 'label' => $this->l('Configuration value'),
-					 'name' => 'MYMODULE_NAME',
-					 'size' => 20,
-					 'required' => true
-				 )
-			 ),
-			 'submit' => array(
-				 'title' => $this->l('Save'),
-				 'class' => 'button'
-			 )
-		 );
-
+		
 		 $helper = new HelperForm();
 
 		 // Module, t    oken and currentIndex
@@ -193,68 +148,219 @@ if (!defined('_PS_VERSION_'))
 			 )
 		 );
 
-		 // Load current value
-		 $helper->fields_value['MYMODULE_NAME'] = Configuration::get('MYMODULE_NAME');
+		  // Init Fields form array
+		 $fields_form[0]['form'] = array(
+			 'legend' => array(
+				 'title' => $this->l('Google Analytics General Settings'),
+			 ),
+			 'input' => array(
+			 //add enable&disable switch
+				  array(
+					'type' => 'switch',
+					'label' => $this->l('Enable Google Analytics Module'),
+					'name' => 'googleanalytics_enable',
+					'is_bool' => true,
+					'required' => true,
+					'values' => array(
+						array(
+							'id' => 'googleanalytics_enable_yes',
+							'value' => 1,
+							'label' => $this->l('Yes'),
+						),
+						array(
+							'id' => 'googleanalytics_enable_no',
+							'value' => 0,
+							'label' => $this->l('No')
+						)
+					),
+					'hint' => $this->l('Enable or disable Google Analytics')
+				),
+			  //add google analytics 
+				array(
+					 'type' => 'text',
+					 'label' => $this->l('Web Tracking Id'),
+					 'name' => 'googleanalytics_webtrackingid',
+					 'size' => 20,
+					 'required' => true,
+					 'hint'=>'Get tracking Id from google'
+				 ),
+			 
+				 array(
+					 'type' => 'radio',
+					 'label' => $this->l('Label1'),
+					 'name' => 'label1',
+					 'size' => 20,
+					 'required' => true,
+					 'values' => array(
+						array(
+							'id' => '1',
+							'value' => '1',
+							'label' => $this->l('111'),
+							'hint' => $this->l('111111'),
+						),
+						array(
+							'id' => '2',
+							'value' => '2',
+							'label' => $this->l('222'),
+							'hint' => $this->l('2222'),
+						),
+					),
+				 ),
+				
+			 ),
+			 'submit' => array(
+				 'title' => $this->l('Save'),
+				 'class' => 'button'
+			 )
+		 );
+		  // Load current value
+		 $helper->fields_value['googleanalytics_enable'] = Configuration::get('googleanalytics_enable');
+		 $helper->fields_value['googleanalytics_webtrackingid'] = Configuration::get('googleanalytics_webtrackingid');
 
+		
 		 return $helper->generateForm($fields_form);
-
+	
 
 
 	 }
-
-	 /**
-	  * hook admin order to send transactions and refunds details
+	 
+	  /**
+	  * back office module configuration page content
 	  */
-	 public function displayAdminOrder()
+	 public function getContent()
 	 {
-
-
-
+		$output = '';
+		if (Tools::isSubmit('submit'.$this->name))
+		{	
+			$error = false;
+			$googleanalytics_enable = Tools::getValue('googleanalytics_enable');
+			$googleanalytics_webtrackingid = Tools::getValue('googleanalytics_webtrackingid');
+						
+			 if(!$error)
+			 {
+				Configuration::updateValue('googleanalytics_enable', $googleanalytics_enable);
+				Configuration::updateValue('googleanalytics_webtrackingid', $googleanalytics_webtrackingid);
+				$output .= $this->displayConfirmation($this->l('Settings updated'));
+			 }
+		}
+		return $output.$this->displayForm();
+	 }
+	 
+	 
+	  /**
+	  * hook page header to add CSS and JS files
+	  */
+	 public function hookDisplayHeader()
+	 {
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook display header</font><br>";
+		
+		if(Configuration::get('googleanalytics_enable')!="" && Configuration::get('googleanalytics_webtrackingid')!=""){
+		
+			$this->context->smarty->assign(
+				  array(
+					  'googleanalytics_webtrackingid' => Configuration::get('googleanalytics_webtrackingid'),
+				  )
+			); 
+			
+			return $this->display(__FILE__, 'addGoogleAnalyticsJs.tpl');	
+		}
+		
 	 }
 
+	 
+	 
 	 /**
+	  * hook top to track transactions
+	  */
+	 public function hookDisplayTop()
+	 {
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook display top</font><br>";
+
+		$controller_name = Tools::getValue('controller');
+
+		if($controller_name=="orderconfirmation"){
+			$orderid = $this->context->controller->id_order;
+			$order = new Order($orderid);
+			$this->context->smarty->assign(
+				  array(
+					  'orderid' => $orderid,
+					  'storename' => $this->context->shop->name,
+					  'grandtotal' => $order->total_paid,
+					  'shipping' => $order->total_shipping,
+					  'tax' => $order->total_paid_tax_incl,
+				  )
+			); 
+			
+		}
+		return $this->display(__FILE__, 'addTransaction.tpl');	
+		
+		
+
+	 }
+	 
+	  /**
 	  * hook footer to load JS script for standards actions such as product clicks
 	  */
-	 public function displayFooter()
+	 public function hookDisplayFooter()
 	 {
-
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook footer</font><br>";
 	 }
 
+	 
 	 /**
 	  * hook home to display generate the product list associated to home featured, news products and best sellers Modules
 	  */
-	 public function displayHome()
-	 {
-
+	 public function hookDisplayHome()
+	 {	
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook home</font><br>";
 
 	 }
-
+	 
+	 
+	 
+	 
 	 /**
 	  * hook product page footer to load JS for product details view
 	  */
-	 public function displayFooterProduct()
+	 public function hookDisplayFooterProduct()
 	 {
-
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook product footer</font><br>";
 
 	 }
 
 	 /**
 	  * hook shopping cart footer to send the checkout details
 	  */
-	 public function displayShoppingCartFooter()
+	 public function hookDisplayShoppingCartFooter()
 	 {
-
+		//verified
+		echo "<br><font color=red size=30px>google analytics hook shopping cart footer</font><br>";
 
 	 }
-
+	 
+	
 	 /**
-	  * hook top to track transactions
+	  * hook admin order to send transactions and refunds details
 	  */
-	 public function displayTop()
+	 public function hookDisplayAdminOrder()
 	 {
+		//verified
+		echo "<font color=red size=30px>google analytics hook admin order</font>";
 
 
 	 }
+
+	 
+
+	 
+
+	 
+
 
 
 
